c0ba76e8a59c2c92d2b7af514039dbb6c60afb5e
Loginov.A <loginov.a@rozetka.com.ua>
2021-11-07 22:18:57 +0200

    Home Task4

---
+++ out/ProjectCar/vehicles/Car.java
@@ -0,0 +1,93 @@
+package vehicles;
+
+import details.Engine;
+import professions.Driver;
+
+public class Car
+{
+                private String brand;
+                private String type;
+                private int weight;
+                private Driver driver;
+                private Engine engine;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Car(String brand, String type, int weight, Driver driver, Engine engine) {
+        this.brand = brand;
+        this.type = type;
+        this.weight = weight;
+        this.driver = driver;
+        this.engine = engine;
+    }
+
+                public void start()
+                {
+                        System.out.println("Let's drive");
+                }
+                public void stop()
+                {
+                        System.out.println("Stop");
+                }
+                public void turnRight()
+                {
+                        System.out.println("Turn right");
+                }
+                public void turnLeft()
+                {
+                        System.out.println("Turn left");
+                }
+                public void printInfo() {
+                    System.out.println(getBrand() + "" + getDriver() + "" + getEngine());
+                }
+
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", type='" + type + '\'' +
+                ", weight=" + weight +
+                ", driver=" + driver +
+                ", engine=" + engine +
+                '}';
+    }
+}
